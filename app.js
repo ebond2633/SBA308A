2b8b4c2d4549f93f0ef97a0d18e31798ca8e5408
Elizabeth Bond <104878451+ebond2633@users.noreply.github.com>
2024-06-20 14:31:37 -0400

    bugged fixed

--- app.js
+++ app.js
@@ -22,15 +22,13 @@ function  createBreedList(breedList){
         const response = await fetch (`https://dog.ceo/api/breed/${breed}/images`)
         const data = await response.json()
         createSlideShow(data.message)
-        console.log(data.message);
         
     }
  }
 
  function createSlideShow(images){
     let currentPosition = 0
-    document.getElementById("slideshow").innerHTML =
-    document.getElementById("slideshow").innerHTML =`
+    document.getElementById("slideShow").innerHTML =`
     <div class="slide" style ="background-image: url('${images[0]}');"></div>
     <div class="slide" style ="background-image: url('${images[1]}');"></div>`
     currentPosition+=2
@@ -38,7 +36,7 @@ setInterval(nextSlide, 3000);
 
 
 function nextSlide(){
-document.getElementById("slideshow").insertAdjacentHTML("beforeend",`<div class="slide" style ="background-image: url('${images[currentPosition]}');"></div>`
+document.getElementById("slideShow").insertAdjacentHTML("beforeend",`<div class="slide" style ="background-image: url('${images[currentPosition]}');"></div>`
 )
 setTimeout(function(){
     document.querySelector(".slide").remove()

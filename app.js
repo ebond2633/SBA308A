51d4f87638860242835b68906e40f25530668431
Elizabeth Bond <104878451+ebond2633@users.noreply.github.com>
2024-06-20 14:20:05 -0400

    fixing bugs

--- app.js
+++ app.js
@@ -22,10 +22,33 @@ function  createBreedList(breedList){
         const response = await fetch (`https://dog.ceo/api/breed/${breed}/images`)
         const data = await response.json()
         createSlideShow(data.message)
+        console.log(data.message);
         
     }
  }
- function createSlideShow(images){
 
- }
+ function createSlideShow(images){
+    let currentPosition = 0
+    document.getElementById("slideshow").innerHTML =
+    document.getElementById("slideshow").innerHTML =`
+    <div class="slide" style ="background-image: url('${images[0]}');"></div>
+    <div class="slide" style ="background-image: url('${images[1]}');"></div>`
+    currentPosition+=2
+setInterval(nextSlide, 3000);
+
+
+function nextSlide(){
+document.getElementById("slideshow").insertAdjacentHTML("beforeend",`<div class="slide" style ="background-image: url('${images[currentPosition]}');"></div>`
+)
+setTimeout(function(){
+    document.querySelector(".slide").remove()
+},1000)
+if(currentPosition + 1 >= images.length){
+    currentPosition = 0
+ } else{
+currentPosition++
+    }
+}
+}
+
 

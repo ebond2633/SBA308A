213dd09960ffaf6a0a5d00fdb3c5bc2989acc23d
Elizabeth Bond <104878451+ebond2633@users.noreply.github.com>
2024-06-20 12:55:17 -0400

    added css

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 async function fetchData(){
-    const response = await fetch ('https://dog.ceo/dog-api/')
+    const response = await fetch ('https://dog.ceo/api/breeds/list/all')
     const data = await response.json()
     createBreedList(data.message)
 }
 
 fetchData()
 function  createBreedList(breedList){
-    document.getElementById("breed").innerHTML=`
+    document.getElementById("breed").innerHTML =`
     
      <select onchange="loadByBreed(this.value)">
      <option>Choose a dog Breed</option>
      ${Object.keys(breedList).map(function (breed){
         return`<option>${breed}</option>`
      }).join("")}
-    </select>    `
+    </select>  
+    `  
 
 }
  async function loadByBreed(breed){ 
     if(breed!="Choose a dog Breed"){
         const response = await fetch (`https://dog.ceo/api/breed/${breed}/images`)
         const data = await response.json()
+        createSlideShow(data.message)
+        
     }
+ }
+ function createSlideShow(images){
+
  }
 

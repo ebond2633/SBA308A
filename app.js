364d34af79410ea778bc32765cd6c8bc4b0ed5bb
Elizabeth Bond <104878451+ebond2633@users.noreply.github.com>
2024-06-21 09:15:26 -0400

    my attempt on modules failed

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+let timer 
+let deleteFirstPhotoDelay
+
 async function fetchData(){
-    const response = await fetch ('https://dog.ceo/api/breeds/list/all')
-    const data = await response.json()
-    createBreedList(data.message)
+    try{
+        const response = await fetch ('https://dog.ceo/api/breeds/list/all')
+        const data = await response.json()
+        createBreedList(data.message)
+    }catch(e){
+    }
+    
 }
 
 fetchData()
-function  createBreedList(breedList){
+
+function createBreedList(breedList){
     document.getElementById("breed").innerHTML =`
     
      <select onchange="loadByBreed(this.value)">
@@ -15,8 +23,8 @@ function  createBreedList(breedList){
      }).join("")}
     </select>  
     `  
-
 }
+
  async function loadByBreed(breed){ 
     if(breed!="Choose a dog Breed"){
         const response = await fetch (`https://dog.ceo/api/breed/${breed}/images`)
@@ -26,19 +34,34 @@ function  createBreedList(breedList){
     }
  }
 
+
  function createSlideShow(images){
     let currentPosition = 0
+    clearInterval(timer)
+    clearTimeout(deleteFirstPhotoDelay)
+   
+
+if(images.length > 1){
     document.getElementById("slideShow").innerHTML =`
     <div class="slide" style ="background-image: url('${images[0]}');"></div>
     <div class="slide" style ="background-image: url('${images[1]}');"></div>`
     currentPosition+=2
-setInterval(nextSlide, 3000);
+    if(images.length == 2)currentPosition =0 
+timer =setInterval(nextSlide, 3000);
+}else{
+    document.getElementById("slideShow").innerHTML =`
+    <div class="slide" style ="background-image: url('${images[0]}');"></div>
+    <div class="slide"></div>`
+    currentPosition+=2
+timer =setInterval(nextSlide, 3000);
+
+}
 
 
 function nextSlide(){
 document.getElementById("slideShow").insertAdjacentHTML("beforeend",`<div class="slide" style ="background-image: url('${images[currentPosition]}');"></div>`
 )
-setTimeout(function(){
+deleteFirstPhotoDelay = setTimeout(function(){
     document.querySelector(".slide").remove()
 },1000)
 if(currentPosition + 1 >= images.length){
@@ -48,5 +71,4 @@ currentPosition++
     }
 }
 }
-
 
---
+++ fetchdata.js
@@ -0,0 +1,34 @@
+import {createSlideShow} from '/app.js'
+
+async function fetchData(){
+    try{
+        const response = await fetch ('https://dog.ceo/api/breeds/list/all')
+        const data = await response.json()
+        createBreedList(data.message)
+    }catch(e){
+    }
+    
+}
+
+fetchData()
+
+function createBreedList(breedList){
+    document.getElementById("breed").innerHTML =`
+    
+     <select onchange="loadByBreed(this.value)">
+     <option>Choose a dog Breed</option>
+     ${Object.keys(breedList).map(function (breed){
+        return`<option>${breed}</option>`
+     }).join("")}
+    </select>  
+    `  
+}
+
+ async function loadByBreed(breed){ 
+    if(breed!="Choose a dog Breed"){
+        const response = await fetch (`https://dog.ceo/api/breed/${breed}/images`)
+        const data = await response.json()
+        createSlideShow(data.message)
+        
+    }
+ }
